1dc25b09a98b38893b50ec03ec32c260c209d7d0
tomatenbrei <tomadroide@gmail.com>
2014-10-24 18:26:25 +0200

    paging of search results added

--- search_style.php
+++ search_style.php
@@ -1,50 +1,110 @@
-<center>
-<div style="display: table-cell;">
 <?php
+session_start();
+
 include("functions.php");
 
-$options = "nothing";
+$options = "l";
+
 if(isset($_GET['o']))
 $options = $_GET['o'];
 
-$limit = "10";
+$options_text = $_GET['t'];
+
+$limit = 10;
+if(isset($_GET['limit']))
+$limit = $_GET['limit'];
+if($limit > 20)
+$limit = 20;
+if($limit < 2)
+$limit = 2;
 
 //when there are no options declared, do this:
-$command = "Select author_id,title,description,url,filesize,frames,views,thumbnail,comments from animations order by date DESC limit ".$limit.";";
+$command = "";
 
 include("ref/mysql_connect.php");
 
+
+$animationcount_q = "select count(*) from animations";
+$result = mysqli_query($link,$animationcount_q);
+$animationcount = 0;
+while ($row = mysqli_fetch_assoc($result)) {
+	$animationcount = $row['count(*)'];
+}
+
+$searchstr = "Result ";
+
+$from = 0;
+if(isset($_GET['from']))
+	$from = intval($_GET['from']);
+if($from > $animationcount)
+	$from = 0;
+if($from < 0)
+	$from = 0;
+
+$to = $from+$limit;
+if($to > $animationcount)
+	$to = $animationcount;
+
 //otherwise, do something of these here:
-if($options != "")
-{
 	if($options == "mv")
 	{
 		//most views
-		$command = "Select author_id,title,description,url,filesize,frames,views,thumbnail,comments from animations order by views DESC limit ".$limit.";";
+		$command = "Select author_id,title,description,url,filesize,frames,views,thumbnail,comments from animations order by views DESC limit $from,$limit;";
+		$searchstr .= " for the most viewed animations from place $from to $to of $animationcount.";
 	}
-	else if($options == "l")
-	{
-		//latest (is default)
-	}
-	else if($options == "n")
+	else if($options == "n" && $options_text != "")
 	{
 		//name
-		$options2 = $_GET['t'];
-		if($options2 != "")
-		{
 			//echo $options2;
-			$command = "Select author_id,title,description,url,filesize,frames,views,thumbnail,comments from animations where title LIKE '%".mysqli_real_escape_string($link,$options2)."%' order by views DESC limit ".$limit.";";
+			$command = "Select author_id,title,description,url,filesize,frames,views,thumbnail,comments from animations where title LIKE '%".mysqli_real_escape_string($link,$options_text)."%' order by views DESC limit $from,$limit;";
 			//echo $command;
-		}
-		
+			$searchstr .= " of animations with the name '$options_text': ";
 	}
-}
+	else
+	{
+		//latest
+		$command = "Select author_id,title,description,url,filesize,frames,views,thumbnail,comments from animations order by date DESC limit $from,$limit;";
+		$searchstr .= " for the latest animations from place $from to $to of $animationcount.";
+	}
 
 //get the animations and display them
 
 $result = mysqli_query($link,$command);
 
+$opt2string = "";
+if($options == "n" && $options_text != "")
+{
+	$newanimationcount_q = "select count(*) from animations where title LIKE '%".mysqli_real_escape_string($link,$options_text)."%';";
+	$result2 = mysqli_query($link,$newanimationcount_q);
+	$newanimationcount = 0;
+
+	while ($row = mysqli_fetch_assoc($result2)) {
+		$newanimationcount = $row['count(*)'];
+	}
+
+	$newto = $to;
+	if($newto > $newanimationcount)
+		$newto = $newanimationcount;
+
+
+	$opt2string = "&t=$options_text";
+	$searchstr.= " found ".$newanimationcount." in ".$animationcount." animations.";
+	$searchstr.= "<br>Showing from ".$from." to ".$newto.".";
+	$animationcount = $newanimationcount;
+}
+
+
+if($to < $animationcount)
+	$searchstr .= "<br><a href = 'index.php?o=$options$opt2string&from=$to&limit=$limit'>foreward</a>";
+if($from > 0)
+	$searchstr .= "<br><a href = 'index.php?o=$options$opt2string&from=".($from-$limit)."&limit=$limit'>backward</a>";
+
+
+echo "<div style='background-color:rgba(255,255,255,0.5);padding:10px;'>$searchstr</div><br>";
+
 $count = 0;
+
+echo "<center>";
 
 while ($row = mysqli_fetch_assoc($result)) {
 	$count++;
@@ -60,7 +120,7 @@ while ($row = mysqli_fetch_assoc($result)) {
 	if($thumbnail == NULL)
 		$thumbnail = "";
 
-	$authorname = "Anonymous";	
+	$authorname = "Anonymous";
 
 	if($authorid != NULL)
 	{
@@ -75,9 +135,7 @@ mysqli_close($link);
 //no animation
 if($count == 0)
 {
-	echo "<div name='animationtable' style='display:inline-block;background-color:white;padding:10px;margin:10px;'>
-<table cellpadding='0' cellspacing='0' border='0'  width='auto'>No Results found.</div>";
+	echo "<div name='animationtable'>No Results found.</div>";
 }
 ?>
-</div>
 </center>
